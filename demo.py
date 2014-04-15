54891c44937cb7e205ad57532b336e0dddd066e6
Daniel Barella <dan.barella@gmail.com>
2014-04-15 19:32:40 -0400

    Added more fun stuff

--- demo.py
+++ demo.py
@@ -5,6 +5,15 @@ and yourself at the top of your programs!
 This is just a demo script to accompany python night, 
 and to remind you of the cool stuff that we taught!
 
+Function-writing stuff
+- First-class functions
+- Comprehensions
+- Named arguments, splats, *args, **kwargs
+
+Class-writing stuff
+- Magic methods
+- Subclassing builtins to do cool stuff
+
 Dan Barella <dbarella@oberlin.edu>
 '''
 
@@ -24,7 +33,7 @@ __version__ = "0.2"
 # Here's a nifty trick:
 DEBUG = False
 
-
+#--- Function-writing stuff. Write better functions ---
 #--- First-class functions ---
 def square(n):
 	'''
@@ -41,11 +50,14 @@ def caller(function, thing):
 	functions are objects. You can pass them around and call them whenever you want
 	'''
 
-	# Because functions are objects, you can make new vars that refer to them
+	# Because functions are objects, you can make new vars that refer to them and call them later
 	f = function
 
 	'''
-	You can check that they point to the same function in memory by doing
+	You can check that they point to the same function in memory by using an assert statement
+
+		(the `is` keyword compares memory addresses)
+
 	Assertions are great for defensive programming, if you want to make sure
 	in one line that something is true, instead of assuming that it is
 		http://en.wikipedia.org/wiki/Assertion_(computing)
@@ -66,7 +78,10 @@ def compose(f, g):
 
 	def result(n):
 		'''
-		Bet you didn't know this was possible (;
+		Bet you didn't know this was possible ;)
+
+		We're defining a new function inside of this function
+			(Sure you can use lambdas but I think they are less readable when being introduced to the idea)
 		'''
 		return f(g(n))
 
@@ -81,19 +96,122 @@ Here's a real-world example from some code I wrote in Compilers (CS331)
 <import regex at the top of the program>
 
 tok_spec = [
-            ('T_NUM',           r'\d+'),                       #Numbers
-            ('T_STRING',        r'"[^"]*"'),                   #Strings
-            ('IGNORE',          r'\s|\n'),                     #Ignore spaces and newlines
-        ]
+			('T_NUM',           r'\d+'),                       #Numbers
+			('T_STRING',        r'"[^"]*"'),                   #Strings
+			('IGNORE',          r'\s|\n'),                     #Ignore spaces and newlines
+		]
 
 tok_regex = '|'.join('(?P<{0}>{1})'.format(*pair) for pair in tok_spec) #Named regex pairs for each item in tok_spec
-get_tok = re.compile(tok_regex).match #Regex match function
+
+#!!! The special sauce !!!
+get_tok = re.compile(tok_regex).match #match is a function in the regex library that takes a string
 
 #Later on you can just call get_tok on a string, and it will return a regex match object
 match_object = get_tok(line)
+
+#This way you don't need to re-compile the regex each time you use it, which takes more memory and slows things down
 '''
 
-#---  ---
+#--- Comprehensions ---
+'''
+Probably one of the most powerful python shorthands
+'''
+
+#--- First-class functions ---
+
+
+#--- Class writing stuff. How to make your classes cooler ---
+#--- Magic Methods ---
+'''
+We'll cover magic methods briefly, but they are amazingly powerful for writing beautiful code.
+Magic methods are all the methods that have double-underscores (dunders) around them
+
+Many smarter people than us have written about this. 
+See http://www.rafekettler.com/magicmethods.html for a great post.
+'''
+class ListWrapper(object):
+	'''
+	I wrote ListWrapper(object) on purpose
+					   ^^^^^^^^
+
+	This is a 'new-style' class definition, different from the old-style definitions
+		which looked like 'class ListWrapper:'
+	It allows you to use both inheritance and the new class methods
+	'''
+
+	def __init__(self, *args):
+		'''
+		__init__ is a magic method, but you always have to define it to do anything useful
+		'''
+		self.data = args
+
+	def __len__(self):
+		'''
+		Called whenever you write `len(thing)`
+		'''
+		return len(self.data)
+
+	def __getitem__(self, key):
+		'''
+		This method is called whenever you write `thing[key]`
+		'''
+		return self.data[key]
+
+	def __setitem__(self, key, value):
+		'''
+		This method is called when you write `thing[key] = value`,
+			and then sets the item at index `key` to be `value`.
+		'''
+		self.data[key] = value
+
+	# Don't use del.
+	# def __delitem__(self, key):
+	# 	'''
+	# 	Called whenever `del thing[key]` is called
+	# 	'''
+	# 	del self.data[key]
+
+	def __add__(self, other):
+		'''
+		Define your own addition. Be careful to return a new object, not to modify self.
+		'''
+		return self.data + other.data
+
+	def __str__(self):
+		'''
+		String representation of this object
+		'''
+		return '{0}'.format(self.data) #Equivalent to str(self.data)
+
+	def __repr__(self):
+		'''
+		Usually called only by the interpreter to represent your object
+		
+		In this case I want it to be the same as __str__
+		'''
+		return str(self) #or you could write self.__str__() or a bunch of other crap
+
+class DefaultDict(dict):
+	'''
+	Here's an example of something that might actually be useful, 
+		via https://www.python.org/download/releases/2.2.3/descrintro 
+	Here we're subclassing the builtin dict object
+	'''
+
+	def __init__(self, default=None):
+		dict.__init__(self)
+		self.default = default
+
+	def __getitem__(self, key):
+		try:
+			return dict.__getitem__(self, key)
+		except KeyError:
+			return self.default
+
+	def get(self, key, *args):
+	    if not args:
+	        args = (self.default,)
+	    return dict.get(self, key, *args)
 
 def main():
 	if DEBUG:
@@ -107,17 +225,49 @@ def main():
 	plus_one_of_square = compose(plus_one, square)
 	print(plus_one_of_square(1))
 
-''' 
-__name__ is a special variable which is set by the python interpreter. 
-It will be '__main__' if your program is being run from the command line,
-	but if your program is being imported by some other script, it will be
-	the name of your program. For example:
+	l = ListWrapper(1, 2, 3)
+	
+	#for loops use the __iter__ method to iterate over the contained objects
+	for item in l:
+		print(item)
 
-<From the python3 interpreter>
->>> import demo
-demo 
+	d = DefaultDict('DEFAULT')
+	d['a'] = 5
+
+	print(d['a']) # 5
+	print(d['b']) # DEFAULT
+
 '''
+This is a good way of defining main() methods and having them only run when you do:
+	python program.py
+
+and not when you do: 
+	import program
+
+from another program'''
+
 if __name__ == '__main__':
 	main()
 
+'''
+Explanation:
+
+__name__ is a special variable which is set by the python interpreter. 
+It will be '__main__' if your program is being run from the command line,
+	but if your program is being imported by some other script, it will be
+	the name of your program. For example:'''
+
 print(__name__)
+
+'''
+<From the python3 interpreter>
+>>> import demo #When imported, this file prints 'demo'
+demo
+
+<From the command line>
+Dan$ python3 demo.py
+__main__
+
+Note: This may not always print demo when imported (imports of imports of imports)
+But when run from the command line it will always print '__main__' 
+'''
